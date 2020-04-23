4230e1b13be5d925dad04f0836a01c86d0451cee
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-04-22 20:52:30 -0700

    processSum passed

--- index.js
+++ index.js
@@ -89,8 +89,9 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `-5`, '-1', and `(num) => num + 1000`,
  * should return 994.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(num1, num2, callback) {
+  let num = num1 + num2;
+  return callback(num);
 }
 
 /**
