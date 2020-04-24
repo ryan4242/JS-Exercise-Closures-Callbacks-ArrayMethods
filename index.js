48efd3757ca901be24a34464c7c1ebb4cffa4629
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-04-23 20:48:30 -0700

    getFullNames passed

--- index.js
+++ index.js
@@ -242,8 +242,9 @@ function stringSmash(strings) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let fullNames = runners.map(runner => runner.first_name + runner.last_name);
+  return fullNames;
 }
 
 /**
